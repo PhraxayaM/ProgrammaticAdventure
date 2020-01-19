ef8ce534b8e91011b71c6da0ade5611a0423f565
MattHew Phraxayavong <mphraxa@gmail.com>
2020-01-19 01:20:09 -0800

    Starting Quizzler Programmatically

---
+++ quizzlerProgrammatic/quizzlerProgrammatic/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  quizzlerProgrammatic
+//
+//  Created by MattHew Phraxayavong on 1/19/20.
+//  Copyright © 2020 Matthew Phraxayavong. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
