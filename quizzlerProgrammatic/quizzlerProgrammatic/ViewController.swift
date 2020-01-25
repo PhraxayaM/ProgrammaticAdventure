93e3da6e52062b60c460fec979cb85a31bf2864b
PhraxayaM <mphraxa@gmail.com>
2020-01-24 22:17:27 -0800

    added models, true/false works and returns the correct answer

--- quizzlerProgrammatic/quizzlerProgrammatic/ViewController.swift
+++ quizzlerProgrammatic/quizzlerProgrammatic/ViewController.swift
@@ -11,9 +11,15 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    let allQuestions = QuestionBank()
+    var pickedAnswer: Bool = false
+    var questionNumber: Int = 0
+    
     override func viewDidLoad() {
         view.backgroundColor = .blue
         setup()
+        let firstQuestion = allQuestions.list[0]
+        questionLabel.text = firstQuestion.questionText
     }
     
     
@@ -34,6 +40,10 @@ class ViewController: UIViewController {
         questionLabel.translatesAutoresizingMaskIntoConstraints = false
         questionLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 200).isActive = true
         questionLabel.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor).isActive = true
+        questionLabel.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor, constant: 20).isActive = true
+        questionLabel.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor, constant: -20).isActive = true
+//        questionLabel.widthAnchor.constraint(equalToConstant: 200).isActive = true
+//        questionLabel.heightAnchor.constraint(equalToConstant: 200).isActive = true
         
         scoreLabel.translatesAutoresizingMaskIntoConstraints = false
         scoreLabel.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
@@ -59,6 +69,8 @@ class ViewController: UIViewController {
         let label = UILabel()
         label.text = "Question"
         label.textColor = .white
+        label.numberOfLines = 3
+        label.textAlignment = .center
         return label
     }()
     
@@ -73,6 +85,8 @@ class ViewController: UIViewController {
         let button = UIButton()
         button.setTitle("True", for: .normal)
         button.backgroundColor = #colorLiteral(red: 0.1921568627, green: 0.6392156863, blue: 0.262745098, alpha: 1)
+        button.addTarget(self, action: #selector(answerPressed), for: .touchUpInside)
+        button.tag = 1
         return button
     }()
     
@@ -80,7 +94,55 @@ class ViewController: UIViewController {
         let button = UIButton()
         button.setTitle("False", for: .normal)
         button.backgroundColor = #colorLiteral(red: 1, green: 0, blue: 0, alpha: 1)
+        button.addTarget(self, action: #selector(answerPressed), for: .touchUpInside)
+        button.tag = 2
         return button
     }()
+    
+    @objc func answerPressed(_ sender: UIButton) {
+        print("button pressed \(Int.random(in: 1..<10))")
+        print("picked answer is \(allQuestions.list[0].answer)")
+        //    print("the true tag is \(trueButton.tag)")
+        //    print("the false tag is \(falseButton.tag)")
+        if sender.tag == 1 {
+            pickedAnswer = true
+        }
+        else if sender.tag == 2 {
+            pickedAnswer = false
+        }
+        checkAnswer()
+        questionNumber = questionNumber + 1
+        nextQuestion()
+    }
+    
+    func checkAnswer() {
+        let correctAnswer = allQuestions.list[questionNumber].answer
+        
+        if correctAnswer == pickedAnswer {
+            print("Correct!")
+        } else {
+            print("Wrong")
+        }
+        
+    }
+    
+    func startOver() {
+        
+    }
+    
+    func nextQuestion() {
+        
+        if questionNumber <= 12 {
+        questionLabel.text = allQuestions.list[questionNumber].questionText
+        } else {
+//            print("Quiz Over")
+            questionNumber = 0
+            let alert = UIAlertController(title: "Awesome!", message: "You've finished all the questions", preferredStyle: .alert)
+            
+            let restartAction = UIAlertAction(title: "Restart", style: .default) { (UIAlertAction) in
+                self.startOver()
+            }
+        }
+    }
     
 }
