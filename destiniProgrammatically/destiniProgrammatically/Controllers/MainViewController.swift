df14aa8788bb22da9e0109c9c8e24c410e915a8e
PhraxayaM <mphraxa@gmail.com>
2020-01-25 09:08:42 -0800

    Destini initial convert to programmatic setup complete

---
+++ destiniProgrammatically/destiniProgrammatically/Controllers/MainViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MainViewController.swift
+//  destiniProgrammatically
+//
+//  Created by MattHew Phraxayavong on 1/25/20.
+//  Copyright © 2020 MattHew Phraxayavong. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        view.backgroundColor = #colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1)
+    }
+    
+}
