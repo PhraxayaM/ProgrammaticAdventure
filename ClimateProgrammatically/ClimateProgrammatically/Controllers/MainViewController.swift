8e2e9cd135d5214e94111825b12a8406b8b40797
PhraxayaM <mphraxa@gmail.com>
2020-01-27 07:39:28 -0800

    conversion to programmatic complete, assets/images imported

---
+++ ClimateProgrammatically/ClimateProgrammatically/Controllers/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ClimateProgrammatically
+//
+//  Created by MattHew Phraxayavong on 1/27/20.
+//  Copyright © 2020 MattHew Phraxayavong. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
+        
+    }
+}
---
+++ ClimateProgrammatically/ClimateProgrammatically/Views/MainView.swift
@@ -0,0 +1,9 @@
+//
+//  MainView.swift
+//  ClimateProgrammatically
+//
+//  Created by MattHew Phraxayavong on 1/27/20.
+//  Copyright © 2020 MattHew Phraxayavong. All rights reserved.
+//
+
+import Foundation
