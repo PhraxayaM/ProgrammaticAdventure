686bd5a172c152a24e1477499267ba46652ad022
PhraxayaM <mphraxa@gmail.com>
2020-02-18 07:22:53 -0800

    initial setup complete, ready to be done programmatically

---
+++ BitCoinTrackeerProgrammatically/BitcoinTrackerProgrammatically/BitcoinTrackerProgrammatically/MainViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MainViewController.swift
+//  BitcoinTrackerProgrammatically
+//
+//  Created by MattHew Phraxayavong on 2/18/20.
+//  Copyright © 2020 MattHew Phraxayavong. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MainViewController: UIViewController {
+    override func viewDidLoad() {
+        view.backgroundColor = #colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)
+    }
+}
